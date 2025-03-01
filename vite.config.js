bac9c606ab5ef8d774a31107fc0d29d54f711669
unknown <ahtashampervaiz6@gmail.com>
2025-03-01 16:28:05 +0500

    set

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repository-name/',  // Add this line to specify the base path
+  base: '/project/',  // Update with your actual repository name
 })
-
